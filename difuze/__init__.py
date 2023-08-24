27d0f53ebde93cf3189c09e8c5b56f55a2b59e0f
Liam <liam.staras@gmail.com>
2023-08-24 21:32:53 +0200

    consistency

--- difuze/__init__.py
+++ difuze/__init__.py
@@ -2,8 +2,6 @@ from . import support
 from . import models
 from . import metrics
 from . import data
-
+from . import log
 from . import training
 from . import inference
-
-import difuze.log as log
