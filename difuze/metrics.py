95e2006165a1a4d5e98089a09f255d20237c31f9
Liam <liam.staras@gmail.com>
2023-08-27 19:41:37 +0200

    simplify metrics interface

--- difuze/metrics.py
+++ difuze/metrics.py
@@ -1,32 +1,5 @@
 import torch
-import numpy as np
 
-from collections.abc import Callable
-from numpy.typing import ArrayLike
-
-class TorchMetric:
-    def __init__(self, metric_fn: Callable[[torch.Tensor, torch.Tensor], float]):
-        self.metric_fn = metric_fn
-    def __call__(self, output: torch.Tensor, target: torch.Tensor) -> tuple[float]:
-        return self._iterate_batches(self.metric_fn, output, target)
-    @staticmethod
-    def _iterate_batches(metric_fn: Callable[[ArrayLike, ArrayLike], tuple], output: ArrayLike, target: ArrayLike):
-        batch_size = len(output)
-        return tuple(
-            metric_fn(output[i].squeeze(), target[i].squeeze()) for i in range(batch_size)
-        )
-    @property
-    def name(self) -> str:
-        return self.metric_fn.name+'->'+self.__class__.__name__
-
-class NumpyMetric(TorchMetric):
-    def __init__(self, metric_fn: Callable[[np.ndarray, np.ndarray], float]):
-        super().__init__(metric_fn=metric_fn)
-
-    @torch.no_grad()
-    def __call__(self, output: torch.Tensor, target: torch.Tensor) -> tuple[float]:
-        return self._iterate_batches(
-            self.metric_fn,
-            output.detach().cpu().numpy(),
-            target.detach().cpu().numpy()
-        )
+class Metric:
+    def __call__(self, predicted_gt_image: torch.Tensor, gt_image: torch.Tensor) -> float:
+        raise AttributeError('Must define how to call metric!')
